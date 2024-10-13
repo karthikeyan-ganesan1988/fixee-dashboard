eff65ccd3f49c2ca1ac6af91c62ef749c20a1ea1
karthikeyan <karthikeyan@zealluxe.com>
2024-10-13 21:50:57 +0530

    changes in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        redirectTo: '/auth/signin',
+        redirectTo: '/analytics',
         pathMatch: 'full'
       },
       {
